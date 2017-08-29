0b01c607a3e5a18c0e7782a439d5e8a7a09a0d4a
Claudio Brasser <brasser13@gmail.com>
2017-08-29 14:16:15 +0200

    linReg example finished, saving classifier to pickle

--- linear_regression/part1.py
+++ linear_regression/part1.py
@@ -1,8 +1,13 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime, time
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 #Get the right data from the dataframe
@@ -26,18 +31,21 @@ print(forecast_out)
 #The label values are the Adj. Close value from the datapoints forecast_out days in the future
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-df.dropna(inplace = True)
+
 
 #Capital X for features
 #Array of all values except label column
 X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
+
+df.dropna(inplace = True)
 #lower case y for lables
 #Array with all values of label column
 y = np.array(df['label'])
 
-X = preprocessing.scale(X)
-y = np.array(df['label'])
 
 #Splits the data into a train and a test part, test part size can be handed
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -45,7 +53,40 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 clf = LinearRegression(n_jobs = 10) #Algorithm used, can be easy switched
 
 #Why train and test on different data ? - Else it already knows the data from training
-clf.fit(X_train, y_train) #train
-accuracy = clf.score(X_test, y_test) #test, accuracy is squared error
+clf.fit(X_train, y_train) #actual training
 
-print(accuracy)
+#Store classifier in Filesystem
+with open('linearRegression.pickle','wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearRegression.pickle','rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(X_test, y_test) #test, accuracy is squared error
+#print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+#Set up for ploting
+last_date = df.iloc[-1].name
+last_unix = time.mktime(last_date.timetuple())
+one_day  = 86400
+next_unix = last_unix + one_day
+
+#
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    #
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
