3b55f14458304525d72570441b32df5f97b32a29
cbrasser <brasser13@gmail.com>
2017-09-02 09:12:27 +0200

    started sentiment analysis

---
+++ tensorflow/posNegSentimentAnalysis/create_sentiment_featuresets.py
@@ -0,0 +1,11 @@
+'''
+lexicon = [chair, table, process, television]
+
+Some random sentence to process
+
+[0     0      0       0    1]
+
+Our lexicon will be consisting out of every word in the entire dataset,
+that way we can build an array of 0s & 1s like above to have uniform datapoints
+and we dont need to worry about difference in sentence length etc.
+'''
